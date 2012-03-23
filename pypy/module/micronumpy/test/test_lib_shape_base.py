587385d3bf2ced206310e301d01d575bd5a89a51
Mike Blume <mike@loggly.com>
2012-03-23 19:58:59 +0000

    contort these tests a bit so they test our stuff

--- pypy/module/micronumpy/test/test_lib_shape_base.py
+++ pypy/module/micronumpy/test/test_lib_shape_base.py
@@ -7,33 +7,33 @@ def assert_equal(a,b):
     else:
         assert result.all()
 
-class TestApplyAlongAxis(BaseNumpyAppTest):
+class AppTestApplyAlongAxis(BaseNumpyAppTest):
     def test_simple(self):
-        from numpy import ones, array_equal, shape
-        from numpy.lib.shape_base import apply_along_axis
+        from numpypy import ones, array_equal, shape
+        from numpypy.lib.shape_base import apply_along_axis
         a = ones((20,10),'d')
         assert array_equal(apply_along_axis(len,0,a),len(a)*ones(shape(a)[1]))
 
     def test_simple101(self,level=11):
-        from numpy import ones, array_equal, shape
-        from numpy.lib.shape_base import apply_along_axis
+        from numpypy import ones, array_equal, shape
+        from numpypy.lib.shape_base import apply_along_axis
         a = ones((10,101),'d')
         assert array_equal(apply_along_axis(len,0,a),len(a)*ones(shape(a)[1]))
 
     def test_3d(self):
-        from numpy import array_equal, arange
-        from numpy.lib.shape_base import apply_along_axis
+        from numpypy import array_equal, arange
+        from numpypy.lib.shape_base import apply_along_axis
         a = arange(27).reshape((3,3,3))
         assert array_equal(apply_along_axis(sum,0,a),
                            [[27,30,33],[36,39,42],[45,48,51]])
 
 
-class TestApplyOverAxes(BaseNumpyAppTest):
+class AppTestApplyOverAxes(BaseNumpyAppTest):
     def test_simple(self):
         '''Had to change the result of this test, which is weird. Got the same
            result with stock NumPy though.'''
-        from numpy import array_equal, arange, array, newaxis
-        from numpy.lib.shape_base import apply_over_axes
+        from numpypy import array_equal, arange, array, newaxis
+        from numpypy.lib.shape_base import apply_over_axes
         a = arange(24).reshape(2,3,4)
         aoa_a = apply_over_axes(sum, a, [0,2])
         expected =array([[14, 16, 18, 20],
@@ -42,10 +42,10 @@ class TestApplyOverAxes(BaseNumpyAppTest):
         assert array_equal(aoa_a, expected)
 
 
-class TestArraySplit(BaseNumpyAppTest):
+class AppTestArraySplit(BaseNumpyAppTest):
     def test_integer_0_split(self):
-        from numpy import arange
-        from numpy.lib.shape_base import array_split
+        from numpypy import arange
+        from numpypy.lib.shape_base import array_split
         a = arange(10)
         try:
             res = array_split(a,0)
@@ -54,8 +54,8 @@ class TestArraySplit(BaseNumpyAppTest):
             pass
 
     def test_integer_split(self):
-        from numpy import arange, array
-        from numpy.lib.shape_base import array_split
+        from numpypy import arange, array
+        from numpypy.lib.shape_base import array_split
         a = arange(10)
         res = array_split(a,1)
         desired = [arange(10)]
@@ -110,16 +110,16 @@ class TestArraySplit(BaseNumpyAppTest):
         compare_results(res,desired)
 
     def test_integer_split_2D_rows(self):
-        from numpy import arange, array
-        from numpy.lib.shape_base import array_split
+        from numpypy import arange, array
+        from numpypy.lib.shape_base import array_split
         a = array([arange(10),arange(10)])
         res = array_split(a,3,axis=0)
         desired = [array([arange(10)]),array([arange(10)]),array([])]
         compare_results(res,desired)
 
     def test_integer_split_2D_cols(self):
-        from numpy import arange, array
-        from numpy.lib.shape_base import array_split
+        from numpypy import arange, array
+        from numpypy.lib.shape_base import array_split
         a = array([arange(10),arange(10)])
         res = array_split(a,3,axis=-1)
         desired = [array([arange(4),arange(4)]),
@@ -130,8 +130,8 @@ class TestArraySplit(BaseNumpyAppTest):
     def test_integer_split_2D_default(self):
         """ This will fail if we change default axis
         """
-        from numpy import arange, array
-        from numpy.lib.shape_base import array_split
+        from numpypy import arange, array
+        from numpypy.lib.shape_base import array_split
         a = array([arange(10),arange(10)])
         res = array_split(a,3)
         desired = [array([arange(10)]),array([arange(10)]),array([])]
@@ -139,8 +139,8 @@ class TestArraySplit(BaseNumpyAppTest):
     #perhaps should check higher dimensions
 
     def test_index_split_simple(self):
-        from numpy import arange, array
-        from numpy.lib.shape_base import array_split
+        from numpypy import arange, array
+        from numpypy.lib.shape_base import array_split
         a = arange(10)
         indices = [1,5,7]
         res = array_split(a,indices,axis=-1)
@@ -148,8 +148,8 @@ class TestArraySplit(BaseNumpyAppTest):
         compare_results(res,desired)
 
     def test_index_split_low_bound(self):
-        from numpy import arange, array
-        from numpy.lib.shape_base import array_split
+        from numpypy import arange, array
+        from numpypy.lib.shape_base import array_split
         a = arange(10)
         indices = [0,5,7]
         res = array_split(a,indices,axis=-1)
@@ -157,8 +157,8 @@ class TestArraySplit(BaseNumpyAppTest):
         compare_results(res,desired)
 
     def test_index_split_high_bound(self):
-        from numpy import arange, array
-        from numpy.lib.shape_base import array_split
+        from numpypy import arange, array
+        from numpypy.lib.shape_base import array_split
         a = arange(10)
         indices = [0,5,7,10,12]
         res = array_split(a,indices,axis=-1)
@@ -167,22 +167,22 @@ class TestArraySplit(BaseNumpyAppTest):
         compare_results(res,desired)
 
 
-class TestSplit(BaseNumpyAppTest):
+class AppTestSplit(BaseNumpyAppTest):
     """* This function is essentially the same as array_split,
          except that it test if splitting will result in an
          equal split.  Only test for this case.
     *"""
     def test_equal_split(self):
-        from numpy import arange
-        from numpy.lib.shape_base import split
+        from numpypy import arange
+        from numpypy.lib.shape_base import split
         a = arange(10)
         res = split(a,2)
         desired = [arange(5),arange(5,10)]
         compare_results(res,desired)
 
     def test_unequal_split(self):
-        from numpy import arange
-        from numpy.lib.shape_base import split
+        from numpypy import arange
+        from numpypy.lib.shape_base import split
         a = arange(10)
         try:
             res = split(a,3)
@@ -191,34 +191,34 @@ class TestSplit(BaseNumpyAppTest):
             pass
 
 
-class TestDstack(BaseNumpyAppTest):
+class AppTestDstack(BaseNumpyAppTest):
     def test_0D_array(self):
-        from numpy import array, array_equal
-        from numpy.lib.shape_base import dstack
+        from numpypy import array, array_equal
+        from numpypy.lib.shape_base import dstack
         a = array(1); b = array(2);
         res=dstack([a,b])
         desired = array([[[1,2]]])
         assert array_equal(res,desired)
 
     def test_1D_array(self):
-        from numpy import array, array_equal
-        from numpy.lib.shape_base import dstack
+        from numpypy import array, array_equal
+        from numpypy.lib.shape_base import dstack
         a = array([1]); b = array([2]);
         res=dstack([a,b])
         desired = array([[[1,2]]])
         assert array_equal(res,desired)
 
     def test_2D_array(self):
-        from numpy import array, array_equal
-        from numpy.lib.shape_base import dstack
+        from numpypy import array, array_equal
+        from numpypy.lib.shape_base import dstack
         a = array([[1],[2]]); b = array([[1],[2]]);
         res=dstack([a,b])
         desired = array([[[1,1]],[[2,2,]]])
         assert array_equal(res,desired)
 
     def test_2D_array2(self):
-        from numpy import array, array_equal
-        from numpy.lib.shape_base import dstack
+        from numpypy import array, array_equal
+        from numpypy.lib.shape_base import dstack
         a = array([1,2]); b = array([1,2]);
         res=dstack([a,b])
         desired = array([[[1,1],[2,2]]])
@@ -227,12 +227,12 @@ class TestDstack(BaseNumpyAppTest):
 """ array_split has more comprehensive test of splitting.
     only do simple test on hsplit, vsplit, and dsplit
 """
-class TestHsplit(BaseNumpyAppTest):
+class AppTestHsplit(BaseNumpyAppTest):
     """ only testing for integer splits.
     """
     def test_0D_array(self):
-        from numpy import array
-        from numpy.lib.shape_base import hsplit
+        from numpypy import array
+        from numpypy.lib.shape_base import hsplit
         a= array(1)
         try:
             hsplit(a,2)
@@ -241,16 +241,16 @@ class TestHsplit(BaseNumpyAppTest):
             pass
 
     def test_1D_array(self):
-        from numpy import array
-        from numpy.lib.shape_base import hsplit
+        from numpypy import array
+        from numpypy.lib.shape_base import hsplit
         a= array([1,2,3,4])
         res = hsplit(a,2)
         desired = [array([1,2]),array([3,4])]
         compare_results(res,desired)
 
     def test_2D_array(self):
-        from numpy import array
-        from numpy.lib.shape_base import hsplit
+        from numpypy import array
+        from numpypy.lib.shape_base import hsplit
         a= array([[1,2,3,4],
                   [1,2,3,4]])
         res = hsplit(a,2)
@@ -258,12 +258,12 @@ class TestHsplit(BaseNumpyAppTest):
         compare_results(res,desired)
 
 
-class TestVsplit(BaseNumpyAppTest):
+class AppTestVsplit(BaseNumpyAppTest):
     """ only testing for integer splits.
     """
     def test_1D_array(self):
-        from numpy import array
-        from numpy.lib.shape_base import vsplit
+        from numpypy import array
+        from numpypy.lib.shape_base import vsplit
         a= array([1,2,3,4])
         try:
             vsplit(a,2)
@@ -272,8 +272,8 @@ class TestVsplit(BaseNumpyAppTest):
             pass
 
     def test_2D_array(self):
-        from numpy import array
-        from numpy.lib.shape_base import vsplit
+        from numpypy import array
+        from numpypy.lib.shape_base import vsplit
         a= array([[1,2,3,4],
                   [1,2,3,4]])
         res = vsplit(a,2)
@@ -281,12 +281,12 @@ class TestVsplit(BaseNumpyAppTest):
         compare_results(res,desired)
 
 
-class TestDsplit(BaseNumpyAppTest):
+class AppTestDsplit(BaseNumpyAppTest):
     """ only testing for integer splits.
     """
     def test_2D_array(self):
-        from numpy import array
-        from numpy.lib.shape_base import dsplit
+        from numpypy import array
+        from numpypy.lib.shape_base import dsplit
         a= array([[1,2,3,4],
                   [1,2,3,4]])
         try:
@@ -296,8 +296,8 @@ class TestDsplit(BaseNumpyAppTest):
             pass
 
     def test_3D_array(self):
-        from numpy import array
-        from numpy.lib.shape_base import dsplit
+        from numpypy import array
+        from numpypy.lib.shape_base import dsplit
         a= array([[[1,2,3,4],
                    [1,2,3,4]],
                   [[1,2,3,4],
@@ -308,28 +308,29 @@ class TestDsplit(BaseNumpyAppTest):
         compare_results(res,desired)
 
 
-class TestSqueeze(BaseNumpyAppTest):
+class AppTestSqueeze(BaseNumpyAppTest):
 
-    @staticmethod
-    def rand(*args):
-        """Returns an array of random numbers with the given shape.
-
-        This only uses the standard library, so it is useful for testing purposes.
-        """
-        import random
-        from numpy.core import zeros, float64
-        results = zeros(args, float64)
-        f = results.flat
-        for i in range(len(f)):
-            f[i] = random.random()
-        return results
 
     def test_basic(self):
-        from numpy import reshape, array_equal, ndarray
-        from numpy.core.fromnumeric import squeeze
-        a = self.rand(20,10,10,1,1)
-        b = self.rand(20,1,10,1,20)
-        c = self.rand(1,1,20,10)
+        from numpypy import reshape, array_equal, ndarray
+        from numpypy.core.fromnumeric import squeeze
+
+        def rand(*args):
+            """Returns an array of random numbers with the given shape.
+
+            This only uses the standard library, so it is useful for testing purposes.
+            """
+            import random
+            from numpypy.core import zeros, float64
+            results = zeros(args, float64)
+            f = results.flat
+            for i in range(len(f)):
+                f[i] = random.random()
+            return results
+
+        a = rand(20,10,10,1,1)
+        b = rand(20,1,10,1,20)
+        c = rand(1,1,20,10)
         assert array_equal(squeeze(a),reshape(a,(20,10,10)))
         assert array_equal(squeeze(b),reshape(b,(20,10,20)))
         assert array_equal(squeeze(c),reshape(c,(20,10)))
@@ -342,10 +343,10 @@ class TestSqueeze(BaseNumpyAppTest):
         assert_equal(type(res), ndarray)
 
 
-class TestKron(BaseNumpyAppTest):
+class AppTestKron(BaseNumpyAppTest):
     def test_return_type(self):
-        from numpy import ndarray, matrix, ones, asmatrix
-        from numpy.lib.shape_base import kron
+        from numpypy import ndarray, matrix, ones, asmatrix
+        from numpypy.lib.shape_base import kron
         a = ones([2,2])
         m = asmatrix(a)
         assert_equal(type(kron(a,a)), ndarray)
@@ -361,10 +362,10 @@ class TestKron(BaseNumpyAppTest):
         assert_equal(type(kron(ma,a)), myarray)
 
 
-class TestTile(BaseNumpyAppTest):
+class AppTestTile(BaseNumpyAppTest):
     def test_basic(self):
-        from numpy import array
-        from numpy.lib.shape_base import tile
+        from numpypy import array
+        from numpypy.lib.shape_base import tile
         a = array([0,1,2])
         b = [[1,2],[3,4]]
         assert_equal(tile(a,2), [0,1,2,0,1,2])
@@ -376,16 +377,16 @@ class TestTile(BaseNumpyAppTest):
                                     [1,2,1,2],[3,4,3,4]])
 
     def test_empty(self):
-        from numpy import array
-        from numpy.lib.shape_base import tile
+        from numpypy import array
+        from numpypy.lib.shape_base import tile
         a = array([[[]]])
         d = tile(a,(3,2,5)).shape
         assert_equal(d,(3,2,0))
 
     def test_kroncompare(self):
         import numpy.random as nr
-        from numpy import ones
-        from numpy.lib.shape_base import tile, kron
+        from numpypy import ones
+        from numpypy.lib.shape_base import tile, kron
         reps=[(2,),(1,2),(2,1),(2,2),(2,3,2),(3,2)]
         shape=[(3,),(2,3),(3,4,3),(3,2,3),(4,3,2,4),(2,2)]
         for s in shape:
@@ -399,6 +400,7 @@ class TestTile(BaseNumpyAppTest):
 
 # Utility
 def compare_results(res,desired):
-    from numpy import array_equal
+    from numpypy import array_equal
     for i in range(len(desired)):
         assert array_equal(res[i],desired[i])
+
